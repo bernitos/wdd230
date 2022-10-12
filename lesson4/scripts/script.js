4b21ebdd642bf769a6b9d3504149d886287e7650
bernitos <49335410+bernitos@users.noreply.github.com>
2022-10-12 17:15:29 +0200

    Update script.js

--- lesson4/scripts/script.js
+++ lesson4/scripts/script.js
@@ -1,4 +1,4 @@
-document.getElementsByClassName('last-updated')[0].innerHTML = "Last Updated: "+document.lastModified
+ document.getElementsByClassName('last-updated')[0].innerHTML = "Last Updated: "+document.lastModified
 
 function toggleMenu() {
     var temp = document.getElementsByClassName("toggler")[0]
@@ -16,17 +16,13 @@ function toggleMenu() {
 
  
 const datefield = document.querySelector(".date");
-const datefieldUK = document.querySelector("aside");  
+ 
 
  
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
-);
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(now);
- 
+); 
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+ 
