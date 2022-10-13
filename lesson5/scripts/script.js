afbaa03a13eec4f3388ac559c7560e62efe3ae84
bernitos <49335410+bernitos@users.noreply.github.com>
2022-10-13 17:33:59 +0200

    Add files via upload

--- lesson5/scripts/script.js
+++ lesson5/scripts/script.js
@@ -1,1 +1,45 @@
+document.getElementsByClassName('last-updated')[0].innerHTML = "Last Updated: "+document.lastModified
 
+function toggleMenu() {
+    var temp = document.getElementsByClassName("toggler")[0]
+    temp.classList.toggle("hide")
+    if (temp.classList[1]=="hide") {
+        document.querySelector(".toggler > a").innerHTML = "&#9776; Menu"
+    }
+    else {
+        document.querySelector(".toggler > a").innerHTML = "&#10006; Close"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+const datefieldUS = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
