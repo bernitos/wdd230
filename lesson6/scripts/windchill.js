1335bba76e8cd556291626c2ecc93fe3bdd9450e
bernitos <49335410+bernitos@users.noreply.github.com>
2022-10-26 09:58:25 +0200

    Update windchill.js

--- lesson6/scripts/windchill.js
+++ lesson6/scripts/windchill.js
@@ -1,1 +1,12 @@
  
+const temp = parseFloat(document.getElementById("temp").textContent);
+const windspeed = parseFloat(document.getElementById("windspeed").textContent);
+const windchill = document.getElementById("windchill");
+var chillvalue = "";
+if (temp <= 10 && windspeed >= 4.8) {
+   chillvalue = (13.12 + 0.6215*temp - 11.37 * (windspeed**0.16) + 0.3965 * temp * (windspeed**0.16)).toFixed(1);
+
+}else{
+    chillvalue = "N/A";
+}
+windchill.textContent = chillvalue + "ºC";
