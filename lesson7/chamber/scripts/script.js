c62ba4d1f0eb047f38b3fbb6bef46bd8b0902ea8
bernitos <49335410+bernitos@users.noreply.github.com>
2022-11-02 10:59:41 +0100

    Update script.js

--- lesson7/chamber/scripts/script.js
+++ lesson7/chamber/scripts/script.js
@@ -35,14 +35,12 @@ if (day == 1 || day == 2) {
 }
 
 
-
-
-
-
-
 // initialize display elements
 const todayDisplay = document.querySelector(".today");
 const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
 
 
 // determine if this is the first visit or display the number of visits.
