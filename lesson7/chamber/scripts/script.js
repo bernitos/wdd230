637b56a821cb2833430b37cf309c57307c697761
bernitos <49335410+bernitos@users.noreply.github.com>
2022-11-04 05:46:44 +0100

    Update script.js

--- lesson7/chamber/scripts/script.js
+++ lesson7/chamber/scripts/script.js
@@ -65,6 +65,20 @@ todayDisplay.textContent = Date.now();
 
 
 
+let date_1 = new Date('10/25/2021');
+let date_2 = new Date();
+
+const days = (date_1, date_2) =>{
+    let difference = date_1.getTime() - date_2.getTime();
+    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+    return TotalDays;
+}
+console.log(days(date_1, date_2) +" days to world cup");
+
+ 
+
+
+
 
 
 
