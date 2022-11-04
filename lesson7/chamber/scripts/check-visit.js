c6cfd5fe19eac21d7adc2afca570149e85ef76aa
bernitos <49335410+bernitos@users.noreply.github.com>
2022-11-04 07:59:15 +0100

    Update check-visit.js

--- lesson7/chamber/scripts/check-visit.js
+++ lesson7/chamber/scripts/check-visit.js
@@ -48,3 +48,78 @@ else {
 // store the this visit as the new last visit
 
 localStorage.setItem("lastVisit", time);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let lastMod = document.querySelector("#lastMod");
+let date = document.querySelector(".date");
+let current = new Date();
+
+let mod = document.lastModified;
+let year = new Date().getFullYear();
+let currentDate = `${mod}`;
+
+function newDate(date){
+ return new Intl.DateTimeFormat("en-UK", {
+  dateStyle: "full",
+}).format(date);
+}
+const formatDate = newDate(current)
+
+if (date) {
+  date.innerHTML = formatDate;
+  lastMod.innerHTML = `&copy; ${year} Marifa chamber of Commerce | Kissi A. Bernitos |Brigham Young University|<br> Last Modified: ${currentDate}`;
+}
+ 
+const day = current.getDay();
+const msg = document.querySelector("#msg");
+
+if (day == 1 || day == 2) {
+  msg.classList.add("show");
+} else {
+  msg.classList.add("hide");
