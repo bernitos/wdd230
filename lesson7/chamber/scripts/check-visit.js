9a6bea7482d62ff58fbb5a9b8dfc8136cc0abc7c
bernitos <49335410+bernitos@users.noreply.github.com>
2022-11-04 09:58:47 +0100

    Update check-visit.js

--- lesson7/chamber/scripts/check-visit.js
+++ lesson7/chamber/scripts/check-visit.js
@@ -1,100 +1,65 @@
 
-// -------------- Visit Difference calculator -------------- 
-// initialize display elements
-const difVisit = document.querySelector(".difVisit");
-const pElement = document.querySelector(".visits p");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	const btn = document.querySelector("#hamBtn");
+const nav = document.querySelector("#headerNav");
 
-const minute = 1000 * 60;
-const hour = minute * 60;
-const day = hour * 24;
-
-let lastVisit = Number(window.localStorage.getItem("lastVisit"));
-const d = new Date();
-const time = d.getTime();
-const dif = (time - lastVisit);
-
-const difSeconds = Math.round(dif/1000);
-const difMinutes = Math.round(dif/minute);
-const difHours = Math.round(dif/hour);
-const difDays = Math.round(dif/day);
-let timeText;
-
-// Saved for later...
-// if(difSeconds <= 120){
-// 	timeText = difSeconds +" seconds ";
-// }
-// else if(difMinutes <= 120){
-// 	timeText = difMinutes +" minutes ";
-// }
-// else if(difHours <= 48){
-// 	timeText = difHours +" hours ";
-// }
-// else{
-// 	timeText = difDays +" days ";
-// }
-
-timeText = difDays +" days ";  
-difVisit.textContent = timeText;
-
-
-// determine if this is the first visit or display the number of visits.
-if (dif !== 0) {
-	difVisit.textContent = timeText;
-} 
-else {
-	pElement.textContent = `This is your first visit!`;
-}
-
-// store the this visit as the new last visit
-
-localStorage.setItem("lastVisit", time);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+btn.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
 
 let lastMod = document.querySelector("#lastMod");
 let date = document.querySelector(".date");
@@ -123,11 +88,9 @@ if (day == 1 || day == 2) {
   msg.classList.add("show");
 } else {
   msg.classList.add("hide");
-	
-	
-	
-	
-	
+}
+
+
 	
 	
 	
