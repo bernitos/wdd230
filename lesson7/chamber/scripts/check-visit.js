117f2e51015c628d8acd47299b01ab05fd573b3e
bernitos <49335410+bernitos@users.noreply.github.com>
2022-11-04 07:49:43 +0100

    Update check-visit.js

--- lesson7/chamber/scripts/check-visit.js
+++ lesson7/chamber/scripts/check-visit.js
@@ -1,8 +1,50 @@
- let lastVisited = window.localStorage.getItem("lastVisited");
-let lastVisitedDisplay = document.querySelector("#lastVisited");
-if (lastVisited) {
-    let days = Math.round((Date.now() - lastVisited));
-    lastVisitedDisplay.textContent = `You visited this page ${days} days ago.`;
-} else {
-    lastVisitedDisplay.textContent = `You visited this page for the first time today.`;
+
+// -------------- Visit Difference calculator -------------- 
+// initialize display elements
+const difVisit = document.querySelector(".difVisit");
+const pElement = document.querySelector(".visits p");
+
+const minute = 1000 * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+let lastVisit = Number(window.localStorage.getItem("lastVisit"));
+const d = new Date();
+const time = d.getTime();
+const dif = (time - lastVisit);
+
+const difSeconds = Math.round(dif/1000);
+const difMinutes = Math.round(dif/minute);
+const difHours = Math.round(dif/hour);
+const difDays = Math.round(dif/day);
+let timeText;
+
+// Saved for later...
+// if(difSeconds <= 120){
+// 	timeText = difSeconds +" seconds ";
+// }
+// else if(difMinutes <= 120){
+// 	timeText = difMinutes +" minutes ";
+// }
+// else if(difHours <= 48){
+// 	timeText = difHours +" hours ";
+// }
+// else{
+// 	timeText = difDays +" days ";
+// }
+
+timeText = difDays +" days ";  
+difVisit.textContent = timeText;
+
+
+// determine if this is the first visit or display the number of visits.
+if (dif !== 0) {
+	difVisit.textContent = timeText;
+} 
+else {
+	pElement.textContent = `This is your first visit!`;
 }
+
+// store the this visit as the new last visit
+
+localStorage.setItem("lastVisit", time);
