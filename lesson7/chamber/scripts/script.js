98c3b4f35ca57f9e24177aaeb9ba79c764cd89d3
bernitos <49335410+bernitos@users.noreply.github.com>
2022-11-05 12:43:45 +0100

    Update script.js

--- lesson7/chamber/scripts/script.js
+++ lesson7/chamber/scripts/script.js
@@ -78,40 +78,6 @@ if (lastVisited) {
 
 
 
-// -------------- Visit Difference calculator -------------- 
-// initialize display elements
-const difVisit = document.querySelector(".difVisit");
-const pElement = document.querySelector(".visits p");
-
-const minute = 1000 * 60;
-const hour = minute * 60;
-const day = hour * 24;
-
-let lastVisit = Number(window.localStorage.getItem("lastVisit"));
-const d = new Date();
-const time = d.getTime();
-const dif = (time - lastVisit);
-
-const difSeconds = Math.round(dif/1000);
-const difMinutes = Math.round(dif/minute);
-const difHours = Math.round(dif/hour);
-const difDays = Math.round(dif/day);
-let timeText;
-
-// Saved for later...
-// if(difSeconds <= 120){
-// 	timeText = difSeconds +" seconds ";
-// }
-// else if(difMinutes <= 120){
-// 	timeText = difMinutes +" minutes ";
-// }
-// else if(difHours <= 48){
-// 	timeText = difHours +" hours ";
-// }
-// else{
-// 	timeText = difDays +" days ";
-// }
-
 timeText = difDays +" days ";  
 difVisit.textContent = timeText;
 
